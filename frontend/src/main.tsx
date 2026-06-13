b1543abd4fe1834b90e14ce50ebd51c220305840
ambikaseth01 <ambikaseth.2006@gmail.com>
2026-06-14 01:30:43 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// ADD THIS
+const theme = localStorage.getItem("theme") || "dark";
+
+if (theme === "dark") {
+  document.documentElement.classList.add("dark");
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
